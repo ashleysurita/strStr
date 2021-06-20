8d04b0a5396795da7d870d947b4547a30b034cb4
Ashley <ashleywong678@gmail.com>
2021-06-19 20:36:57 -0400

    Create my_solution.js

---
+++ my_solution.js
@@ -0,0 +1,17 @@
+function(haystack, needle) {
+    if(!haystack.includes(needle)) return -1
+    if(!needle.length) return 0
+    
+    let i = 0
+    while(i < haystack.length){
+        
+        if(haystack.slice(i, i + needle.length) === needle){
+            console.log('if statement')
+            return i
+        } else {
+            i++            
+        }                  
+    }
+    
+    return i
+};
